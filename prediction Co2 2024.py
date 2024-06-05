bf4cf8aee4d51da161f77c78bcc00e0391e5e47b
Zozioo <zoe.rouq@gmail.com>
2024-06-05 08:26:28 +0200

    traitement toutes données: enlever les colonnes superflues

---
+++ prediction Co2 2024.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import csv
+
+
+#collecte des données 
+
+energie = pd.read_csv('data\energie production .csv')
+prix_elec = pd.read_csv('data\prix electricité par an.csv')
+prix_petrole = pd.read_csv('data\prix pétrole.csv')
+quotas = pd.read_csv('data\prix quota Europe annuel.csv')
+
+
+
+
+
+
+
