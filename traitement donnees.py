6d4b2ad8ce42c212f48ae7164358b10f225c1026
Zozioo <zoe.rouq@gmail.com>
2024-06-05 10:00:57 +0200

    final data ajouté

--- traitement donnees.py
+++ traitement donnees.py
@@ -1,10 +1,12 @@
 import pandas as pd
-file_path = 'data\prix petrole.csv'
+file_path = 'data\prix quota Europe annuel.csv'
 
 data = pd.read_csv(file_path)
 
-data['Month'] =pd.to_datetime(data['Month'], format='%d/%m/%Y')
-data['Year'] = data['Month'].dt.year
+data = data.drop('Exchange Rate (USD)')
 
-grouped_data = data.groupby('Year')['Europe Brent Spot Price FOB Dollars per Barrel'].mean().reset_index()
-grouped_data.to_csv('data\prix pétrole.csv', index=False)
+data = data.drop('EUR_USD')
+
+data = data.drop('EUR_EUR')
+
+data.to_csv(file_path)
