60e6056bfc437cfc682343f87940e422aa45326a
Zozioo <zoe.rouquie@clovis.fr>
2024-06-13 19:04:56 +0200

    affichage et traitement de l'electricité par mois

--- affichage.py
+++ affichage.py
@@ -2,18 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data =  pd.read_csv('data/final data 2024.csv')
+data =  pd.read_csv('data/production electricite.csv')
 colors = [
     "#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f",
     "#bcbd22","#17becf","#1e90ff","#ff1493","#32cd32","#ff4500","#9932cc","#ffd700"
 ]
 
-for index, c in enumerate(data.drop(columns='Year').columns):
-    plt.figure()  # Crée un nouveau graphique
-    plt.plot(data['Year'], data[c], marker='o', label=c, color=colors[index])
-    plt.title(c)  # Définit le titre du graphique comme le nom de la variable
-    plt.xlabel('Année')
-    plt.ylabel('Valeur')
-    plt.legend()
-    plt.show()
+plt.figure(figsize=(10, 6))
+
+# Tracer chaque colonne sur le même graphique
+for index, c in enumerate(data.drop(columns=['Total']).columns):
+    plt.plot(data['date'], data[c], marker='o', label=c, color=colors[index % len(colors)])
+
+# Ajouter des titres et des légendes
+plt.title('Évolution des différentes sources d\'énergie')
+plt.xlabel('Mois')
+plt.ylabel('Valeur')
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  # Légende à l'extérieur du graphique
+
+# Afficher le graphique
+plt.tight_layout()
+plt.show()
 
--- traitement donnees.py
+++ traitement donnees.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
-file_path = 'data\bilan conjoncturel EU ETS datas - data.csv'
+file_path = 'data\production elec mensuelle.csv'
 
 data = pd.read_csv(file_path, sep = ';')
 
 
-data['Year'] = pd.to_datetime(data['date']).dt.year
+df_pivot = data.pivot_table(index='date', columns='variable', values='generation_twh', aggfunc='sum').reset_index()
 
-# Utiliser pivot_table pour restructurer les données
-df_pivot = data.pivot_table(index='Year', columns='variable', values='generation_twh', aggfunc='sum').reset_index()
-
-# Afficher le DataFrame résultant
-
-df_pivot.to_csv('data\data final.csv',index= False)
+df_pivot.to_csv('data\production electricite.csv',index= False)
 print('done')
+
