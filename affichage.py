6c81003d0d55a87d08d207d271cf85c13942906d
Zozioo <zoe.rouquie@clovis.fr>
2024-06-13 17:10:07 +0200

    prevision sur toute l'année 2024

--- affichage.py
+++ affichage.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data =  pd.read_csv('data/final data T1.csv')
+data =  pd.read_csv('data/final data 2024.csv')
 colors = [
     "#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f",
     "#bcbd22","#17becf","#1e90ff","#ff1493","#32cd32","#ff4500","#9932cc","#ffd700"
--- prediction Co2 2024.py
+++ prediction Co2 2024.py
@@ -2,13 +2,13 @@ import csv
 
 def data_to_trimestre(value):
     try:
-        return float(value) * 0.6 if value else ""
+        return float(value) * 4 if value else ""
     except ValueError:
         return value
 
 cols = ['emission de CO2', 'Bioenergy', 'Clean', 'Coal', 'Fossil', 'Gas', 'Hydro', 'Nuclear', 'Other Fossil', 'Other Renewables', 'Solar', 'Wind', 'Wind and solar']
 
-with open('data/final data.csv', mode='r', newline='') as csvfile:
+with open('data/final data T1.csv', mode='r', newline='') as csvfile:
     reader = csv.reader(csvfile)
     header = next(reader) 
 
@@ -26,7 +26,7 @@ with open('data/final data.csv', mode='r', newline='') as csvfile:
             new_row = row
         new_rows.append(new_row)
 
-with open('final data x4.csv', mode='w', newline='') as csvfile:
+with open('final data 2024.csv', mode='w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(new_rows)
 
